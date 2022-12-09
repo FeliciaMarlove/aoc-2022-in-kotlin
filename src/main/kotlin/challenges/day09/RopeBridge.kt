9f95838c271ac52b232d8253327614389345d409
aivcp <aivcp@goiba.net>
2022-12-09 13:50:57 +0100

    Day 9 Part 1 RopeBridge (ongoing)
    - Nothing so far

---
+++ src/main/kotlin/challenges/day09/RopeBridge.kt
@@ -0,0 +1,15 @@
+package challenges.day09
+
+import java.io.File
+
+class RopeBridge {
+    companion object {
+        private val input = "./src/main/kotlin/challenges/day09/input.txt"
+
+        fun computeResult() {
+            File(input).forEachLine {
+
+            }
+        }
+    }
+}
